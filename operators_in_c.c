ce750a02e4bd94711db8c3d967b464c6cd3ca58e
cecil <82148488+cecvl@users.noreply.github.com>
2023-05-31 10:58:14 +0300

    logic operators

---
+++ operators_in_c.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+main(){
+ 
+    //logical operators
+    //boolean
+    bool x = true;
+    bool y = false; // because this is false x&&y will be zero
+    printf("xx && yy = %d", x&&y);
+
+    return 0;
+
+}
+//need to read more
