2afd9c1440a8b00f2af779628fe022d18f77befe
cecil <82148488+cecvl@users.noreply.github.com>
2023-06-05 23:27:37 +0300

    string functions

---
+++ string_functions.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+    char food[] = "Pizza";
+
+    printf("%zu", strlen(food)); //5
+
+    char bestFood[strlen(food)]; 
+
+    strcpy(bestFood, food);
+
+    printf("\n%s", bestFood); //Pizza
+
+    printf("\n%zu", strlen(bestFood)); //5
+    
+    return 0;
+}
---
+++ string_functions2.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+    char string1[20], string2[20];
+
+    printf("Enter two words:");
+
+    fgets(string1, sizeof(string1), stdin); //input two strings
+    fgets(string2, sizeof(string2), stdin);
+
+    if (strlen(string1) > strlen(string2)) //use strlen() to get length of strings
+    {
+        printf("The larger string is: %s", string1);
+    }
+    else{
+        printf("The larger string is: %s", string2);
+    }
+
+    return 0;
+}
