12a08990202e2da8da136f3add3f4ee6cf7afa70
cecil <82148488+cecvl@users.noreply.github.com>
2023-06-05 15:37:55 +0300

    Access string characters via index

--- strings.c
+++ strings.c
@@ -1,11 +1,21 @@
 #include <stdio.h>
 #include <string.h>
-
+/* this program takes your full name as input and prints your name.
+Then it changes the first letter to X and prints your name.
+*/
 int main()
 {
-    char str[] = "Program";
+    char str[20];
 
-    printf("%s", str[0]);
+    printf("Enter your name:");
+
+    fgets(str, sizeof(str), stdin);
+
+    printf("%s", str);
+
+    str[0] = 'X'; //changes index 0 to X
+
+    printf("%s", str);
     
 
     return 0;
