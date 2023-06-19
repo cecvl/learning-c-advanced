286000f0dad47b526b31f1e161dc9b80dd60364e
cecil <82148488+cecvl@users.noreply.github.com>
2023-06-19 21:01:51 +0300

    assign

--- structures_union.c
+++ structures_union.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 
 
 /* struct is a collection of variables (can be of different types) under a single name
@@ -6,5 +7,20 @@
 
 
 struct Person{
+    int age;
     
-    }person1, person2, p[20];
+    }person1;
+
+   int main()
+    {
+        printf("Enter your age : ");
+        //fgets(person1.birthdate, sizeof(person1.birthdate), stdin);
+        scanf("%d", &person1.age);
+        //strcpy(person1.month, "October");
+        
+        printf("AGE %d", person1.age);
+
+
+        return 0;
+    };
+    
