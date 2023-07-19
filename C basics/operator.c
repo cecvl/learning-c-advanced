a703794b33fb9fe0790f0f5803d888e96bce4bfd
Cecil <cecioasuna@gmail.com>
2023-07-20 01:22:39 +0300

    typedef

---
+++ C basics/operator.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main()
+{
+    int a = 5;
+    if (!(a == 6)) //the condtion in the bracket is false, the ! operator makes it true
+    {
+     printf("haha");   
+    }
+}
---
+++ C basics/typedef.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+//example 1
+typedef struct{
+  float price ;
+  char name[20];  
+}menuitems;
+
+
+//example 2
+typedef struct menuitems{
+    float price;
+    char name[20]
+}menuitems;
+
+
