264976bb42e5413b2ee063f856f719adfd2ed472
Christian Cecil <cecioasuna@gmail.com>
2023-09-23 16:11:27 +0300

    Update arrays.c

--- arrays/arrays.c
+++ arrays/arrays.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 
-//datatype arrayName[arraySize];
 
 int main(){
    int age[5] = {21, 21, 23, 34, 45 };//assign values
+   double amount[] = {2.2, 2.3, 4.1, 4.5};
 
    age[2] = 33; //assigning a new value using index
+   amount[3] = 2.2;
    printf("%d %d %d", age[2],age[3],age[4]); //arrayName[index]
-
+   printf("%f", amount[3]); 
 
 }
 
